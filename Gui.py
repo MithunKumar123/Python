7b62a67a8a25b9a2444ba29edeb2c9b5c128a14b
Mithunkumar123 <mithun.selvaraj@gmail.com>
2023-07-01 23:57:55 +0530

    Todo app Gui changes - Error handling

--- Gui.py
+++ Gui.py
@@ -1,6 +1,9 @@
 import function
 import PySimpleGUI as psg
+import time
 
+psg.theme("Purple")
+clock = psg.Text('',key='clock')
 label = psg.Text("Type in a todo")
 input_box = psg.InputText(tooltip="Enter todo",
                           key='todo')
@@ -9,7 +12,8 @@ list_box = psg.Listbox(values=function.get_todos(), key='todos', enable_events=T
 edit_button = psg.Button("Edit")
 complete_button = psg.Button("Complete")
 exit_button = psg.Button("Exit")
-layout = [[label],
+layout = [[clock],
+          [label],
           [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button]]
@@ -18,8 +22,8 @@ window = psg.Window("My Todo App",
                     layout= layout,
                     font=('Helvetica', 21))
 while True:
-    event, values = window.read()
-
+    event, values = window.read(timeout=1000)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case 'Add':
             todos = function.get_todos()
@@ -27,20 +31,26 @@ while True:
             todos.append(new_todo)
             function.write_todos(todos)
         case 'Edit':
-            todo_to_edit = values["todos"][0]
-            new_todo = values['todo']
-            todos = function.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + "\n"
-            function.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values['todo']
+                todos = function.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + "\n"
+                function.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                psg.popup("Please select some options",title="Warning", line_width=50, font=('Helvetica', 21))
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = function.get_todos()
-            todos.remove(todo_to_complete)
-            function.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = function.get_todos()
+                todos.remove(todo_to_complete)
+                function.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                psg.popup("Please select some options", title="Warning", line_width=50, font=('Helvetica', 21))
         case 'Exit':
             break
         case 'todos':
