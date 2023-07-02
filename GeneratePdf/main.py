6f7a30b1da2d0fb3197e3152b4a6c8ba43bc4551
Mithunkumar123 <mithun.selvaraj@gmail.com>
2023-07-02 23:41:19 +0530

    pdf generator - footer and line generator

--- GeneratePdf/main.py
+++ GeneratePdf/main.py
@@ -2,6 +2,11 @@ from fpdf import FPDF
 import pandas as p
 
 
+def line_generator(x1,y1,x2):
+    for co_ord in range(y1,300,10):
+        pdf.line(x1,co_ord,x2, co_ord)
+
+
 def add_footer(footer_message):
     pdf.ln(265)
     pdf.set_font(family="Times", style="B", size=12)
@@ -12,6 +17,7 @@ def add_footer(footer_message):
 def add_subsequent_pages(page_title, no_of_pages):
     for i in range(no_of_pages):
         pdf.add_page()
+        line_generator(10, 20, 200)
         add_footer(page_title)
 
 
@@ -20,7 +26,7 @@ def add_first_page(page_title):
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=page_title, align="L", ln=1)
-    pdf.line(10, 20, 200, 20)
+    line_generator(10, 20, 200)
     add_footer(page_title)
 
 
