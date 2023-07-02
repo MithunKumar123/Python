94c631dcebe47bb4d1c1345f186ea7b718d1cc75
Mithunkumar123 <mithun.selvaraj@gmail.com>
2023-07-02 12:28:52 +0530

    Portfolio- Rendered all the details from csv to screen

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -3,7 +3,16 @@ import pandas as p
 
 sl.set_page_config(layout="wide")
 
-col_1,col_2 = sl.columns(2)
+col_1, col_2 = sl.columns(2)
+
+
+def show_column(data_list):
+    for index_arg, row_arg in data_list.iterrows():
+        sl.header(row_arg['title'])
+        sl.write(row_arg["description"])
+        sl.image("images/" + row_arg['image'])
+        sl.write(f"[Source Code]({row_arg['url']})")
+
 
 with col_1:
     sl.image("images/photo.png")
@@ -15,12 +24,10 @@ with col_2:
 
 sl.write("Below you can find some apps which I built in python, Feel free to contact me")
 
-col_3, col_4 = sl.columns(2)
+col_3, empty_col, col_4 = sl.columns([1.5,0.5,1.5])
 data = p.read_csv("data.csv", sep=";")
 with col_3:
-    for index, row in data[:10].iterrows():
-        sl.header(row['title'])
+    show_column(data[:10])
 
 with col_4:
-    for index, row in data[10:].iterrows():
-        sl.header(row['title'])
+    show_column(data[10:])
