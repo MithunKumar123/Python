23930b02d6ac9a9e6a5fece6355087b1f4dc72e7
Mithunkumar123 <mithun.selvaraj@gmail.com>
2023-07-02 11:19:08 +0530

    WebUI module added

--- WebUI.py
+++ WebUI.py
@@ -13,7 +13,12 @@ def add_todo():
 st.title("My Todo App")
 st.subheader("This is my web todo app")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add a new Todo...", on_change=add_todo, key='new_todo')
+st.text_input(label="To avoid exception",label_visibility='hidden', placeholder="Add a new Todo...", on_change=add_todo, key='new_todo')
