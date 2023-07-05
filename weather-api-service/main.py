b2919b4f15ec04787776f1ad082dc91358ac3ce7
Mithunkumar123 <mithun.selvaraj@gmail.com>
2023-07-05 11:40:39 +0530

    Weather data api - Home page update

--- weather-api-service/main.py
+++ weather-api-service/main.py
@@ -23,6 +23,12 @@ def details(station, date):
         "temperature": temperature
     }
 
+@app.route("/api/v1/<station>")
+def station_details(station):
+    file_name = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    content = p.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+    result = content.to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
